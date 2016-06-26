d6df9e5435bd4de12fd9eb3bfd3f1b834bb27f04
reuthi <reuthi1@gmail.com>
2016-06-26 15:24:19 +0300

    ticket

--- js/booking.js
+++ js/booking.js
@@ -1,37 +1,87 @@
 'use strict';
 
+const KEY_TICKETS = 'tickets';
+
 
 $(document).ready(()=>{
+    
+    Ticket.renderSrcs();     
 
-    rendersrcs();
-    new Ticket ();  
 });
 
-function Ticket () {
-    this.src = Ticket.getSrc();
+
+function Ticket (src, dest) {
+    this.src = src;
+    this.dest = dest;
+    this.date = '';
 }
 
- Ticket.getSrc = function () {
-    $('#bsrc').on('change', function(e) {
-        console.log(this.value) 
-    });
+// Ticket.save = function (fromObj) {
+//     // let src = 
+//     // let dest = ;
+// }
+
+Ticket.query = function () {
+
+    if (Ticket.tickets) return Ticket.tickets;
+    let jsonTickets = Ticket.loadJSONFromStorage();
+
+    Ticket.tickets = jsonTickets.map(jsonTickets => {
+        return new Passenger(jsonTickets.src, jsonTickets.dest);
+    })
+
+    return Ticket.tickets;
 }
 
-function rendersrcs () {
-    $('#bsrc').val(getSrcs());
+Ticket.loadJSONFromStorage = function () {
+    let tickets = getFromStorage(KEY_TICKETS);
+    if (!tickets) tickets = [];
+    return tickets;
+}
+
+Ticket.BookIt = function () {
+    var formObj = $('form').serializeJSON();
+    console.log(formObj)
+    let tickets = Ticket.query();
+    let ticket = new Ticket (formObj.bsrc, formObj.bdest);
+    tickets.push(ticket);
+    Ticket.tickets = tickets;
+    saveToStorage(KEY_TICKETS, tickets);
 }
 
 
-function getSrcs () {
+$('#bsrc').on('change', function() {
+    let src = this.value;
+    let dest = Ticket.renderDests(src);
+    
+    // Ticket.save (src, dest);
+
+});
+
+Ticket.renderSrcs = function  () {
     let flights = Flight.query();  
-    
-    var strHTML = flights.map(flight => {
+    let strHTML = `<option value=""></option>`
+    strHTML += flights.map(flight => {
     return `<option value="${flight.src}"> ${flight.src} </option>`     
-    }).join(' ');    
+    }).join(' ');  
     
-    console.log(strHTML);   
     $('#bsrc').html(strHTML);
 }
 
+Ticket.renderDests = function (src) {
+
+    let flights = Flight.query();  
+
+    flights = flights.filter(flight => flight.src !== src);
+
+    let strHTML = `<option value=""></option>`
+
+    strHTML += flights.map(flight => {
+    return `<option value="${flight.destination}"> ${flight.destination} </option>`     
+    }).join(' ');  
+    
+    
+    $('#bdest').html(strHTML);
+}
 
 
