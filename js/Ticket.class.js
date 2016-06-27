6f6026c0ea387a08596f4d8595784b2a2b5b3f84
reuthi <reuthi1@gmail.com>
2016-06-27 09:08:27 +0300

    booking

--- js/Ticket.class.js
+++ js/Ticket.class.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const KEY_TICKETS = 'tickets';
-// const PHOTO_SRC = 'img/passengers/';
-
 
 $(document).ready(()=>{
     
@@ -14,7 +12,6 @@ $(document).ready(()=>{
 function Ticket (src, dest, date, avilableSits) {
     this.src = src;
     this.dest = dest;
-    // this.id = id;
     this.date = date;
     this.avilableSits = avilableSits;
 }
@@ -37,66 +34,6 @@ Ticket.loadJSONFromStorage = function () {
     return tickets;
 }
 
-Ticket.Search = function () {
-    var formObj = $('form').serializeJSON();
-    let flight = Ticket.getFlight(formObj.bsrc, formObj.bdest);
-    let flightAvilableSits = Ticket.getAvilableSits(flight)
-    console.log(flightAvilableSits)
-    
-    let tickets = Ticket.query();
-    let ticket = new Ticket (formObj.bsrc, formObj.bdest, flight.date, flightAvilableSits);
-    tickets.push(ticket);
-    Ticket.tickets = tickets;
-    saveToStorage(KEY_TICKETS, tickets);
-    Ticket.renderFlight(flight.date, flightAvilableSits)
-}
-
-Ticket.getFlight = function (src, dest) {
-    let flights = Flight.query();  
-    let flight = flights.filter(flight => flight.src === src && flight.dest === dest); 
-    return flight[0];
-}
-
-Ticket.getAvilableSits = function (flight) {
-        console.log(flight)
-    console.log(flight.plane)    
-    let planes = Plane.query();
-    console.log(planes);    
-    let plane = planes.filter(plane => plane.model === flight.plane);
-    console.log(plane[0])
-    return plane[0].sitsCount;
-}
-
-Ticket.renderFlight = function (date, avilableSits) {
-    console.log(date, avilableSits);
-    $('.flightContainer').css('display', 'block')
-   $('.dateContaineText').html(date);
-    $('.planeContainerText').html('sits count: ' + avilableSits)  
-}
-
-
-// TODO: anonymos func:
-$('#bsrc').on('change', function() {
-    let src = this.value;
-    Ticket.renderDests(src);
-    Ticket.findFlightById
-});
-
-$('#bpass').on('change', function() {
-    console.log(this.value)
-    let passengerName = this.value;
-    let passengers = Passenger.query();
-    // console.log(passengers)
-    passengers = passengers.filter(p => p.name === passengerName);
-    // console.log(passengers)
-    // console.log
-    (passengers[0].id).append('');
-    
-     $('.imgPassCont').attr('src',PHOTO_SRC + passengers[0].id + '.jpg');
-
-    // passengers[0].id
-    
-});
 
 Ticket.renderSrcs = function  () {
     let flights = Flight.query();  
@@ -111,13 +48,8 @@ Ticket.renderSrcs = function  () {
 
 Ticket.renderDests = function (src) {
     let flights = Flight.query();  
-
     flights = flights.filter(flight => flight.src === src);
     
-    console.log(flights)
-    
-    // let id = Ticket.findFlightById(flights)
-
     let strHTML = `<option value=""></option>`
     strHTML += flights.map(flight => {
     return `<option value="${flight.dest}"> ${flight.dest} </option>`     
@@ -126,11 +58,52 @@ Ticket.renderDests = function (src) {
     $('#bdest').html(strHTML);
 }
 
-// Ticket.findFlightById = function (flight) {
-//     let flights = Flight.query();  
-//     flights = flights.filter(flight => flight.src === src);
-//     return id = flight[0].id;
-// }
+Ticket.Search = function () {
+    var formObj = $('form').serializeJSON();
+    let flight = Ticket.getFlight(formObj.bsrc, formObj.bdest);
+    let flightAvilableSits = Ticket.getAvilableSits(flight)
+    
+    let tickets = Ticket.query();
+    let ticket = new Ticket (formObj.bsrc, formObj.bdest, flight.date, flightAvilableSits);
+    tickets.push(ticket);
+    Ticket.tickets = tickets;
+    
+    saveToStorage(KEY_TICKETS, tickets);
+    Ticket.renderFlight(flight.date, flightAvilableSits)
+}
+
+Ticket.renderFlight = function (date, avilableSits) {
+    $('.flightContainer').css('display', 'block')
+    $('.dateContaineText').html(date);
+    $('.planeContainerText').html('sits count: ' + avilableSits)  
+}
+
+Ticket.getFlight = function (src, dest) {
+    let flights = Flight.query();  
+    let flight = flights.filter(flight => flight.src === src && flight.dest === dest); 
+    return flight[0];
+}
+
+Ticket.getAvilableSits = function (flight) {   
+    let planes = Plane.query(); 
+    let plane = planes.filter(plane => plane.model === flight.plane);
+    return plane[0].sitsCount;
+}
+
+
+// TODO: anonymos func:
+$('#bsrc').on('change', function() {
+    let src = this.value;
+    Ticket.renderDests(src);
+    Ticket.findFlightById
+});
+
+$('#bpass').on('change', function() {
+    let passengerName = this.value;
+    let passengers = Passenger.query();
+    passengers = passengers.filter(p => p.name === passengerName);
+     $('.imgPass').attr('src',PHOTO_SRC + passengers[0].id + '.jpg');
+});
 
 
 Ticket.BookIt = function () {
@@ -140,9 +113,6 @@ Ticket.BookIt = function () {
 
 Ticket.renderPass = function () {
     let passengers = Passenger.query();    
-    // let passengersNames = passengers.map(p => {
-    //    return p.name
-    // })
     
     let strHTML = `<option value=""></option>`
         strHTML += passengers.map(p => {
@@ -152,3 +122,7 @@ Ticket.renderPass = function () {
     $('#bpass').html(strHTML);
 
 };
+
+Ticket.savePass = function () {
+    
+}
